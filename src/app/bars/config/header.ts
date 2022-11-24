44d8a3438f99b877e0a157e349c30a93e5e2d6d3
tibame201020 <tibame201020@gmail.com>
2022-11-24 00:57:41 +0000

    env config

--- src/app/bars/config/header.ts
+++ src/app/bars/config/header.ts
@@ -1,26 +1,5 @@
 import { Bar } from '../../model/bar';
 
 export const HEADER: Bar = {
-  alink: [
-    {
-      name: 'Test',
-      link: '/test',
-      icon: 'terminal',
-    },
-    {
-      name: 'Solution',
-      link: '/solution',
-      icon: 'data_thresholding',
-    },
-    {
-      name: 'Edit',
-      link: '/edit',
-      icon: 'token',
-    },
-    {
-      name: 'Setting',
-      link: '/setting',
-      icon: 'settings',
-    },
-  ],
+  alink: [],
 };
---
+++ src/app/model/record.ts
@@ -0,0 +1,22 @@
+export interface Record {
+  id: number;
+  category: string;
+  recordTime: number;
+  item: string;
+  price: number;
+  unit: string;
+  count: number;
+  ps: string;
+}
+
+export interface RecordForm {
+  filterKey: string;
+  category: string;
+  startTime: number;
+  endTime: number;
+  item: string;
+  price: number;
+  unit: string;
+  count: number;
+  ps: string;
+}
